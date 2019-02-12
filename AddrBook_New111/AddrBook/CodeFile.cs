634f911ba1edf0397e7452d0c76963859ce77b40
gkwp <gkwp@hanmail.net>
2019-02-12 09:48:42 +0900

    111

---
+++ AddrBook_New111/AddrBook/CodeFile.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlTypes;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace AddrBook
+{
+   
+    public class Common_DB
+    {
+        //-----------------------------------------------------------------------------
+        // DataBase Connection
+        //-----------------------------------------------------------------------------
+        public static OleDbConnection DBConnection()
+        {
+            OleDbConnection Conn;
+            //아래는 오라클용 접속 문자열, data source 애는 tnsnames.ora 파일에 있는 Alias명을 넣으면 됩니다.
+            //32비트 
+            //string ConStr = "Provider=MSDAORA;data source=onj;User ID=scott;Password=tiger";
+
+            //64비트
+            string ConStr = @"Provider=SQLOLEDB;Data Source=192.239.13.6;Initial Catalog=test;user id=itmaster;Password=!t@dmin100$krc96(^";
+
+            Conn = new OleDbConnection(ConStr);
+            return Conn;
+        }
+
+
+        //-----------------------------------------------------------------------------
+        // DataSelect 
+        //-----------------------------------------------------------------------------
+        public static OleDbDataReader DataSelect(string sql, OleDbConnection Conn)
+        {
+            try
+            {
+                OleDbCommand myCommand = new OleDbCommand(sql, Conn);
+                return myCommand.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                //Log File에 출력 
+
+                MessageBox.Show(sql + "\n" + ex.Message, "DataSelect");
+                return null;
+            }
+            finally
+            {
+            }
+        }
+
+
+        //-----------------------------------------------------------------------------
+        // DataDelete, DataInsert 
+        //-----------------------------------------------------------------------------
+        public static bool DataManupulation(string sql, OleDbConnection Conn)
+        {
+            try
+            {
+                OleDbCommand myCommand = new OleDbCommand(sql, Conn);
+                myCommand.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log File에 출력 
+
+                MessageBox.Show(sql + "\n" + ex.Message, "DataManupulation");
+                return false;
+            }
+            finally
+            {
+
+            }
+        }
+    }
+ 
+
+
+}
---
+++ AddrBook_New111/AddrBook/FrmLogViewer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AddrBook
+{
+    public partial class FrmLogViewer : Form
+    {
+        public FrmLogViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                
+                txtLog.Text = sr.ReadToEnd();
+
+                sr.Close();
+            }
+        }
+    }
+}
